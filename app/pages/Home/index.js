5de0ddb5b87a4efec42104a05bf19cc4fb49792f
Pablo Sichert <mail@pablosichert.de>
2017-03-24 17:43:36 +0100

    Apply some styling

--- app/pages/Home/index.js
+++ app/pages/Home/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import styles from './Home.css';
 
 export default class Home extends Component {
     render() {
         return (
             <div>
-                Home
+                <h1 className={styles.Heading}>CSS Modules Demo</h1>
                 <ul>
                     <li>
                         <Link to='/foo'>foo</Link>
