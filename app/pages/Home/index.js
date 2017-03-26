b7b5e6ce2f99ac0f41ff130fbd7fff56664c5670
Pablo Sichert <mail@pablosichert.de>
2017-03-27 00:23:19 +0200

    Center stuff

--- app/pages/Home/index.js
+++ app/pages/Home/index.js
@@ -5,16 +5,18 @@ import styles from './Home.css';
 export default class Home extends Component {
     render() {
         return (
-            <div>
-                <h1 className={styles.Heading}>CSS Modules Demo</h1>
-                <ul>
-                    <li>
-                        <Link to='/foo'>foo</Link>
-                    </li>
-                    <li>
-                        <Link to='/bar'>bar</Link>
-                    </li>
-                </ul>
+            <div className={styles.Home}>
+                <div>
+                    <h1 className={styles.Heading}>CSS Modules Demo</h1>
+                    <ul>
+                        <li>
+                            <Link to='/foo'>foo</Link>
+                        </li>
+                        <li>
+                            <Link to='/bar'>bar</Link>
+                        </li>
+                    </ul>
+                </div>
             </div>
         );
     }
