90ad677c63f9c73e29e14af7be4592d80c886bb8
Pablo Sichert <mail@pablosichert.de>
2017-03-27 14:32:14 +0200

    Add example of class names not clashing

--- app/pages/Bar/index.js
+++ app/pages/Bar/index.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import { browserHistory as history } from 'react-router';
 import Page from '../../components/Page';
 import Button from '../../components/Button';
+import styles from './Bar.css';
 
 export default class Bar extends Component {
     render() {
         return (
             <Page>
-                Bar
-                <br />
+                <h1 className={styles.Heading}>Bar with class .Heading</h1>
                 <br />
                 <Button onClick={history && history.goBack}>←</Button>
             </Page>
--- app/pages/Foo/index.js
+++ app/pages/Foo/index.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import { browserHistory as history } from 'react-router';
 import Page from '../../components/Page';
 import Button from '../../components/Button';
+import styles from './Foo.css';
 
 export default class Foo extends Component {
     render() {
         return (
             <Page>
-                Foo
-                <br />
+                <h1 className={styles.Heading}>Foo with class .Heading</h1>
                 <br />
                 <Button onClick={history && history.goBack}>←</Button>
             </Page>
