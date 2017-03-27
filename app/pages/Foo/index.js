ba59f764e6cf6a4decacb170dda538942cdb0ebb
Pablo Sichert <mail@pablosichert.de>
2017-03-27 11:45:23 +0200

    Add back buttons

--- app/pages/Foo/index.js
+++ app/pages/Foo/index.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 import Page from '../../components/Page';
+import Button from '../../components/Button';
 
 export default class Foo extends Component {
     render() {
         return (
             <Page>
                 Foo
+                <br />
+                <br />
+                <Link to='/'>
+                    <Button>←</Button>
+                </Link>
             </Page>
         );
     }
